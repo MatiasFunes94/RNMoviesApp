006bfd86e6d03e0c87c6839b6ceeb432bd5f651f
MatiFunes94 <mfunes@midasconsultores.com.ar>
2021-05-22 16:59:03 -0300

    remove apikey

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -1,25 +1,26 @@
 import axios from 'axios';
+import { apiKey } from './key';
 
 export const movieDB = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/movie',
-    params: {
-        api_key: '0d99d3efe9d677cf3d562abcfa677825',
-        append_to_response: 'videos,images',
-    }
+  baseURL: 'https://api.themoviedb.org/3/movie',
+  params: {
+    api_key: apiKey,
+    append_to_response: 'videos,images',
+  }
 })
 
 export const genresDB = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/genre/movie',
-    params: {
-        api_key: '0d99d3efe9d677cf3d562abcfa677825',
-        language: 'en-US',
-    }
+  baseURL: 'https://api.themoviedb.org/3/genre/movie',
+  params: {
+    api_key: apiKey,
+    language: 'en-US',
+  }
 })
 
 export const serieDB = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/tv',
-    params: {
-        api_key: '0d99d3efe9d677cf3d562abcfa677825',
-        append_to_response: 'videos,images',
-    }
+  baseURL: 'https://api.themoviedb.org/3/tv',
+  params: {
+    api_key: apiKey,
+    append_to_response: 'videos,images',
+  }
 })
